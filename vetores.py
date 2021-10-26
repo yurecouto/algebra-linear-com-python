94e5407ee5510ba0c85d24347d274a1cf32c1e22
Yure Barbosa Couto <49762865+yurecouto@users.noreply.github.com>
2021-10-26 16:44:51 -0300

    commit 01 - 26/10

---
+++ vetores.py
@@ -0,0 +1,15 @@
+from math import sqrt as raiz
+
+'''def cos_theta(v1, v2):
+    a = v1 * v2
+   ''' 
+
+
+'''teta = 0.5
+k = 2
+b = k ** 2
+       
+test = k / (raiz(2) * raiz(4 + (k ** 2)))
+
+print(test)'''
+
