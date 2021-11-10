d3400f3cb4e62bc37e300779210740e5b78d7d82
Yure Barbosa Couto <49762865+yurecouto@users.noreply.github.com>
2021-11-10 17:25:09 -0300

    final commit

--- algebra/combinacao_linear.py
+++ algebra/combinacao_linear.py
@@ -30,5 +30,47 @@ def comb_linear(vetor_main, *vetores):
     
     return False
 
+'''print(comb_linear([1, 2, 4], [1, 2, 1], [1, 5, 2], [1, 1, 0]))'''
 
-print(comb_linear([1, 2, 4], [1, 2, 1], [1, 5, 2], [1, 1, 0]))
+
+# Verifica se os vetores são linearmente independentes ou dependentes
+def dependencia_linear(*vetores):   
+
+    # Funciona com base em tentativa e erro 
+    # a variavel teste é uma lista com vários conjuntos de possíveis escalares
+    lenght = len(vetores)
+    testes = geradora(10, lenght)
+    zero_soma = list()
+    zero_escalares = list()
+
+    # vetor zero para ser igualado
+    for z in vetores[0]:
+        zero_soma.append(0)
+
+    # escalares zero
+    for z in vetores:
+        zero_escalares.append(0)
+
+    # Verifica uma tentativa
+    for teste in testes:
+
+        lista = list()
+
+        # Multiplica cada vetor por uma tentativa
+        for index in range(0, len(teste)):
+            lista.append(multiplicar_vetor(vetores[index], teste[index]))
+
+        soma = soma_vetores_lista(lista)
+
+        print(f'{teste} ===> {soma}\n')
+        
+    
+        if soma == zero_soma:
+            if teste == zero_escalares:
+                return 'LI'
+            
+            else:
+                return 'LD'
+
+print(dependencia_linear([1, 0], [1, 1]))
+
--- algebra/testes.py
+++ algebra/testes.py
@@ -1,43 +1,11 @@
-a = [5, -4]
-b = [5, -4]
 
-print(a+b)
+def teste(n):
+    lista = list()
+    for c in range(0, n):
+        for t in range(-10, 11):
+            lista.append(t)
 
-'''
-    lenght = len(vetores)
-    errors = list()
-    produto_principal = produto_escalar(vetor_main, vetor_main)
+    for n in range(0, len(lista), 10):
+        return n
 
-    # Primeiro criamos valores que serão testados via tentativa e erro
-    while True:
-        teste = sample(range(-10, 10), lenght)
-
-        if teste not in errors:
-            vetores_teste = list()
-            produtos_escalares = list()
-            
-            # Aqui multiplicamos os vetores pelos valores gerados anterioirmente
-            for index in range(0, lenght):
-                vetor_teste = multiplicar_vetor(vetores[index], teste[index])
-                vetores_teste.append(vetor_teste)
-
-            # Fazemos o produto escalar de cada vetor 
-            for vetor_teste in vetores_teste:
-                pe = produto_escalar(vetor_teste, vetor_teste)
-                produtos_escalares.append(pe)
-                    
-            # Soma dos produtos escalares        
-            soma = sum(produtos_escalares)
-
-            if soma == produto_principal:
-                return True
-
-            # Adicionamos o conjunto de tentativa aos erros (se estiver na lista de erros, não será calculado)
-            errors.append(teste)
-
-            vetores_teste.clear()
-            produtos_escalares.clear()
-
-        else:    
-            print(teste)
-'''
+print(teste(5))
